72e26133620d3f191facc64c61b33427f4943a65
nsretkovicc <nikolasretkovic@webelinx.com>
2023-07-01 02:39:32 +0200

    Rename function sum to neighbour_sum
    - Only "sum" as a function name was shadowing built-in function "sum"

--- exam4.py
+++ exam4.py
@@ -2,11 +2,11 @@
 # elementi zbirovi susednih elementa liste.
 # Primer: zbir([1, 2, 3, 4, 5]) = [3, 5, 7, 9]
 
-# Using the Python programming language, write a function named 'sum',
+# Using the Python programming language, write a function named 'neighbour_sum',
 # which creates a new list where each element is the sum of neighboring elements from the original list.
 # Example: sum([1, 2, 3, 4, 5]) should return [3, 5, 7, 9]
 
-def sum(numbers:list):
+def neighbour_sum(numbers:list):
     return [numbers[i] + numbers[i+1] for i in range(len(numbers) - 1)]
 
-print(sum([1, 2, 3, 4, 5]))
+print(neighbour_sum([1, 2, 3, 4, 5]))
