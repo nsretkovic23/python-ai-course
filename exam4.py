c642b9f0a08866017eeef78de27c54e98fdce09d
nsretkovicc <nikolasretkovic@webelinx.com>
2023-07-01 02:21:52 +0200

    Complete

---
+++ exam4.py
@@ -0,0 +1,12 @@
+﻿# Korišcenjem programskog jezika Python, napisati funkciju zbir, koja kreira novu listu čiji su
+# elementi zbirovi susednih elementa liste.
+# Primer: zbir([1, 2, 3, 4, 5]) = [3, 5, 7, 9]
+
+# Using the Python programming language, write a function named 'sum',
+# which creates a new list where each element is the sum of neighboring elements from the original list.
+# Example: sum([1, 2, 3, 4, 5]) should return [3, 5, 7, 9]
+
+def sum(numbers:list):
+    return [numbers[i] + numbers[i+1] for i in range(len(numbers) - 1)]
+
+print(sum([1, 2, 3, 4, 5]))
